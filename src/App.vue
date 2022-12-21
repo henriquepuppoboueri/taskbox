0d85275b6c81fb5887b95fb7f5bbbeba9f8fdec9
henriquepuppoboueri <henrique.puppo@ufjf.br>
2022-12-21 17:16:13 -0300

    syncing

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <InboxScreen />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import InboxScreen from './components/InboxScreen.vue';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
-  }
+    InboxScreen,
+  },
 };
 </script>
 
 <style>
-@import "./index.css";
+@import './index.css';
 </style>
